d522aaaf45333de860173f9e6d4f5b45aa132885
bofafa <bopuinar@gmail.com>
2025-03-20 17:46:43 +0800

    Added new files

--- js/shopping.js
+++ js/shopping.js
@@ -1,3 +1,5 @@
+//const { button } = require("framer-motion/client");
+
 // const { p } = require("framer-motion/client");
 
 // let output;
@@ -63,64 +65,113 @@
 // console.log(second_li);
 
 // multiple item selectuon
-const listItems = document.querySelectorAll(".item");
-console.log(listItems[1].innerText);
+// const listItems = document.querySelectorAll(".item");
+// console.log(listItems[1].innerText);
 
 
-listItems[1].style.color = "red";
-listItems.forEach((item, index)=>{
-  item.style.color="red";
-  if(index==1){
-    item.remove();
-  }
-  if(index == 0){
-    item.innerText = "Oats";
-    item.innerHTML = `Oats <button class="remove-item btn-link text-red">
-  <i class="fa-solid fa-xmark"></i> 
-</button>`;
-}
-});
+// listItems[1].style.color = "red";
+// listItems.forEach((item, index)=>{
+//   item.style.color="red";
+//   if(index==1){
+//     item.remove();
+//   }
+//   if(index == 0){
+//     item.innerText = "Oats";
+//     item.innerHTML = `Oats <button class="remove-item btn-link text-red">
+//   <i class="fa-solid fa-xmark"></i> 
+// </button>`;
+// }
+// });
 
 
-// get element by class name
-const item2 = document.getElementsByClassName("item");
-console.log (item2);
-const item3 = document.getElementsByTagName("li");
-console.log(item3);
+ // get element by class name
+// const item2 = document.getElementsByClassName("item");
+// console.log (item2);
+// const item3 = document.getElementsByTagName("li");
+// console.log(item3);
 
 
-const item3List = Array.from(item3);
-console.log(item3List);
+// const item3List = Array.from(item3);
+// console.log(item3List);
 
-item3List.forEach((item)=> {console.log(item.innerText)
+// item3List.forEach((item)=> {console.log(item.innerText)
 
-});
+// });
 
-const parent = document.querySelector(".container");
-console.log(parent.children);
-console.log(parent.firstElementChild);
-console.log(parent.lastElementChild);
-console.log(parent.firstElementChild.nextElementSibling.parentElement);
+// const parent = document.querySelector(".container");
+// console.log(parent.children);
+// console.log(parent.firstElementChild);
+// console.log(parent.lastElementChild);
+// console.log(parent.firstElementChild.nextElementSibling.parentElement);
 
-const parent2 = document.querySelector(".container");
-console.log(parent2.childNodes);
-child = parent2.childNodes;
-console.log(child[0].nodeName);
-console.log(child[0].textContent);
-console.log(child[1].textContent);
-console.log(child[1].outerHTML);
-console.log(parent2.firstChild.nextSibling.parentNode);
-console.log(parent2.lastChild.previousSibling.parentElement);
+// const parent2 = document.querySelector(".container");
+// console.log(parent2.childNodes);
+// child = parent2.childNodes;
+// console.log(child[0].nodeName);
+// console.log(child[0].textContent);
+// console.log(child[1].textContent);
+// console.log(child[1].outerHTML);
+// console.log(parent2.firstChild.nextSibling.parentNode);
+//console.log(parent2.lastChild.previousSibling.parentElement);
 
-// create element
-const div = document.createElement("div");
-div.className ="my-element";
-div.setAttribute("title", "My Element");
-div.id = "my-element"
-div.innerText = "Hello World";
+//create element
+// const div = document.createElement("div");
+// div.className ="my-element";
+// div.setAttribute("title", "My Element");
+// div.id = "my-element"
+// div.innerText = "Hello World";
 
 //create node
-const text = document.createTextNode("hello");
-div.appendChild(text);
-document.body.appendChild(div);
-console.log(document.body);
+// const text = document.createTextNode("hello");
+// div.appendChild(text);
+// document.body.appendChild(div);
+//console.log(document.body);
+
+// create item old way
+function createListItem(item){
+  const li = document.createElement("li");
+  li.innerHTML = `${item}
+  <button class ="remove-item btn-link text-red">
+  <i class ="fa-solid fa-xmark"></i></button>
+  `;
+
+document.querySelector(".items").appendChild(li);
+}
+createListItem("egg");
+
+
+// create item
+function createNewItem(item){
+  // step 1 : create element
+  const li = document.createElement("li");
+  //step 2: create next node
+  //step 3: append text node into element
+  li.appendChild(document.createTextNode(item));
+  //step 4: create button element
+  const button = document.createElement("button");
+  button.className = "remove-item btn-link text-red";
+  const icon = document.createElement("i");
+  icon.className = "fa-solid fa-xmark";
+  //step 5: build component one by one
+  button.appendChild(icon);
+  li.appendChild(button);
+  document.querySelector(".items").appendChild(li);
+
+}
+createNewItem("cherry");
+
+createNewItem("cake");
+createNewItem("soda");
+
+function create3Item(item){
+  const li = document.createElement("li");
+li.appendChild(document.createTextNode(item));
+const button = document.createElement("button");
+button.className = "remove-item btn-link text-red";
+const icon = document.createElement("i");
+icon.className ="fa-solid fa-xmark"
+button.appendChild(icon);
+li.appendChild(button);
+document.querySelector(".items").appendChild(li);
+}
+create3Item("beer");
