41059b00e675e6d5337b14567f5188833fbbf910
bofafa <bopuinar@gmail.com>
2025-03-27 17:35:16 +0800

    Added new files

--- react/App.js
+++ react/App.js
@@ -1,11 +1,22 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.age),
+  ]);
+};
+
 const App = () => {
   return React.createElement(
     "div",
     {},
-    [React.createElement("h1", {}, "Hello"),
-      React.createElement("h2", null , "world"),
-    ]);
+    [
+      React.createElement("h1", {}, "Hello"),
+      React.createElement(Pet, { name: "John", age: 10 }),
+      React.createElement(Pet, { name: "Peter", age: 20 }),
+    ]
+  );
 };
+
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(React.createElement(App));
